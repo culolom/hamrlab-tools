7363863f8f34a7d06c310cecd6e77ae4233f7728
culolom <58364027+culolom@users.noreply.github.com>
2025-11-18 16:59:32 +0800

    Update logout.py

--- hamrlab_tools/auth/logout.py
+++ hamrlab_tools/auth/logout.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from hamster_tools.auth.session import logout
+from hamrlab_tools.auth.session import logout
 
 
 def logout_button():
     if st.sidebar.button("🚪 登出"):
         logout()
         st.rerun()
+
